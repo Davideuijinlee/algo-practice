c0c729b197c2709ca2438f128b3a83bbcd78f50c
David Lee <davideuijinlee@gmail.com>
2020-10-27 20:50:56 -0700

    quick sort added

--- index.js
+++ index.js
@@ -29,6 +29,21 @@ function merge(arr1, arr2) {
     return results
 }
 
+function pivot(arr, start=0, end=arr.length+1) {
+    var pivot = arr[start];
+    var swapIndex = start;
+
+    for (let i = start + 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            swapIndex++;
+            swap(i, swapIndex, arr);
+        }
+    }
+
+    swap(start, swapIndex, arr);
+    return swapIndex;
+}
+
 //sorting algorithms
 
 function bubbleSort(arr) {
@@ -87,4 +102,24 @@ function mergeSort(arr) {
     let right = mergeSort(arr.slice(mid));
 
     return merge(left, right);
+}
+
+function quickSort(arr, left=0, right=arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+
+        quickSort(arr, left, pivotIndex - 1);
+    
+        quickSort(arr, pivotIndex + 1, right);
+    }
+
+    return arr;
+}
+
+function testArray(indexNum) {
+    let results = [];
+    results[indexNum] = 1;
+    
+
+    return results;
 }
